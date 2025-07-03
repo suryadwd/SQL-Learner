16972b9cd719d3d38c87ec42b73d35857b430ad8
Suryakant <surajdwivedi644@gmail.com>
2025-07-04 05:24:44 +0530

    day 1

--- day1.sql
+++ day1.sql
@@ -1,6 +1,78 @@
-use MyDatabase;
-
-SELECT
-*
+/* 
+SELECT * 
 FROM
-customers;
+dbo.orders;
+
+SELECT 
+order_date, sales
+FROM 
+orders;
+
+SELECT 
+first_name
+FROM 
+customers
+where
+score > 500;
+
+SELECT 
+*
+FROM 
+customers
+
+SELECT 
+first_name
+FROM 
+customers
+where 
+score != 0;
+
+SELECT 
+first_name
+FROM 
+customers
+where 
+country = 'Germany';
+
+SELECT 
+*
+FROM 
+customers
+ORDER BY 
+country  desc,score asc;
+
+SELECT 
+country, 
+sum(score) AS Tanya
+FROM
+customers
+GROUP BY country;
+
+SELECT 
+country,
+count(country) AS customer,
+sum(score) AS countryscores
+FROM 
+customers
+GROUP BY country
+
+*/
+
+SELECT 
+country,
+count(country) AS customer,
+sum(score) AS countryscores
+FROM 
+customers
+GROUP BY country
+HAVING sum(score) > 800;
+
+
+
+
+
+
+
+
+
+
